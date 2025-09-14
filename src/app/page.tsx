78c637b0dd73d7f05d2ca20a29675d2a17cf7a42
SADIK <139070345+square-story@users.noreply.github.com>
2025-09-14 08:30:49 +0530

    fix: remove the education and work section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -150,7 +150,7 @@ export default function Page() {
           </Markdown>
         </BlurFade>
       </section>
-      <section id="work">
+      {/* <section id="work">
         <div className="flex min-h-0 flex-col gap-y-3">
           <BlurFade delay={BLUR_FADE_DELAY * 5}>
             <h2 className="text-lg sm:text-xl font-bold">Work Experience</h2>
@@ -197,7 +197,7 @@ export default function Page() {
             </BlurFade>
           ))}
         </div>
-      </section>
+      </section> */}
       <section id="skills">
         <div className="flex min-h-0 flex-col gap-y-3">
           <BlurFade delay={BLUR_FADE_DELAY * 9}>
