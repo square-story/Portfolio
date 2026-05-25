772b387efb252351a6583530d292485b6fd5f447
SADIK <gibmepreo@gmail.com>
2026-05-25 11:20:13 +0530

    fix: correct font family syntax in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
           defaultTheme="system"
         >
           <TooltipProvider delayDuration={0}>
-            <div className="flex min-h-screen w-full flex-col font-[family-name:var(--font-inter-tight)]">
+            <div className="flex min-h-screen w-full flex-col font-(family-name:--font-inter-tight)">
               <div className="relative mx-auto w-full max-w-screen-sm flex-1 px-4 pt-20">
                 <Header />
                 {children}
